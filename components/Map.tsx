e93dcbf7f0ebe9564133c14eddc8584f7e407c8d
Hristo Bogdanov <hristobogdanov03@pmgkk.com>
2024-09-15 00:09:36 +0300

    fix map rerender bug

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,28 +1,28 @@
-import { icons } from "@/constants";
-import { calculateRegion, generateMarkersFromData } from "@/lib/map";
-import { useDriverStore, useLocationStore } from "@/store";
-import { MarkerData } from "@/types/type";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
 import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 
-export default function Map() {
+import { icons } from "@/constants";
+import { useFetch } from "@/lib/fetch";
+import {
+  calculateDriverTimes,
+  calculateRegion,
+  generateMarkersFromData,
+} from "@/lib/map";
+import { useDriverStore, useLocationStore } from "@/store";
+import { Driver, MarkerData } from "@/types/type";
+
+const directionsAPI = process.env.EXPO_PUBLIC_DIRECTIONS_API_KEY;
+
+const Map = () => {
   const {
     userLongitude,
     userLatitude,
-    destinationLongitude,
     destinationLatitude,
+    destinationLongitude,
   } = useLocationStore();
-
   const { selectedDriver, setDrivers } = useDriverStore();
-
-  const [markers, setMarkers] = useState<MarkerData[]>([]);
-
-  const region = calculateRegion({
-    userLatitude,
-    userLongitude,
-    destinationLatitude,
-    destinationLongitude,
-  });
 
   const drivers = [
     {
@@ -71,6 +71,9 @@ export default function Map() {
     },
   ];
 
+  // const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
+  const [markers, setMarkers] = useState<MarkerData[]>([]);
+
   useEffect(() => {
     if (Array.isArray(drivers)) {
       if (!userLatitude || !userLongitude) return;
@@ -83,7 +86,46 @@ export default function Map() {
 
       setMarkers(newMarkers);
     }
-  }, [drivers, userLatitude, userLongitude]);
+  }, []);
+
+  useEffect(() => {
+    if (
+      markers.length > 0 &&
+      destinationLatitude !== undefined &&
+      destinationLongitude !== undefined
+    ) {
+      calculateDriverTimes({
+        markers,
+        userLatitude,
+        userLongitude,
+        destinationLatitude,
+        destinationLongitude,
+      }).then((drivers) => {
+        setDrivers(drivers as MarkerData[]);
+      });
+    }
+  }, [markers, destinationLatitude, destinationLongitude]);
+
+  const region = calculateRegion({
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  });
+
+  // if (loading || (!userLatitude && !userLongitude))
+  //   return (
+  //     <View className="flex justify-between items-center w-full">
+  //       <ActivityIndicator size="small" color="#000" />
+  //     </View>
+  //   );
+
+  // if (error)
+  //   return (
+  //     <View className="flex justify-between items-center w-full">
+  //       <Text>Error: {error}</Text>
+  //     </View>
+  //   );
 
   return (
     <MapView
@@ -96,7 +138,7 @@ export default function Map() {
       showsUserLocation={true}
       userInterfaceStyle="light"
     >
-      {markers.map((marker) => (
+      {markers.map((marker, index) => (
         <Marker
           key={marker.id}
           coordinate={{
@@ -105,10 +147,39 @@ export default function Map() {
           }}
           title={marker.title}
           image={
-            selectedDriver === marker.id ? icons.selectedMarker : icons.marker
+            selectedDriver === +marker.id ? icons.selectedMarker : icons.marker
           }
         />
       ))}
+
+      {destinationLatitude && destinationLongitude && (
+        <>
+          <Marker
+            key="destination"
+            coordinate={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            title="Destination"
+            image={icons.pin}
+          />
+          <MapViewDirections
+            origin={{
+              latitude: userLatitude!,
+              longitude: userLongitude!,
+            }}
+            destination={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            apikey={directionsAPI!}
+            strokeColor="#0286FF"
+            strokeWidth={2}
+          />
+        </>
+      )}
     </MapView>
   );
-}
+};
+
+export default Map;
